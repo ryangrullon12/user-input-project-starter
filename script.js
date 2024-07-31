96882ad1f2e04fe835cc69b59aa5867e2fb5d847
ryangrullon12 <ryangrullon123@gmail.com>
2024-07-31 15:53:53 -0400

    function did not work

---
+++ script.js
@@ -0,0 +1,17 @@
+let main_container = document.getElementsByClassName("main_container");
+let form_container = document.getElementsByClassName("form_container");
+let adlib = document.getElementsByClassName("adlib");
+let main_title = document.getElementById("main_title");
+
+function sentence(event){
+    event.preventDefault();
+    let userInput = main_container.value
+    let src;
+    if (userInput == value) {
+        console.log = "Lets play a crazy pizza exercise";
+    }
+    else (userInput.value);
+        console.log = "Lets play a crazy pizza exercise";
+    }
+//I dont understand the function part
+form_container.addEventListener("click", sentence);
